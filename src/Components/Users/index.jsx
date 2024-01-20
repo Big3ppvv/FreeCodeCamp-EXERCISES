2a60fbc722c6c4b742b638a0a2cc65330086efb9
Big3ppv <joaquinruizperez2002@gmail.com>
2024-01-20 10:39:04 -0300

    Add new json file and new lines of code

--- src/Components/Users/index.jsx
+++ src/Components/Users/index.jsx
@@ -1,17 +1,31 @@
-const Users = (props) => {
+import { useState, useEffect } from 'react'
+
+const Users = () => {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    const getData = async () => {
+      const response  = await fetch('users.json');
+      console.log('setting')
+      setUsers( await response.json());
+    }
+  
+    getData()
+  },[])
+  
+
   return (
     <div>
-      {props.freeCodeCampUser.map((userData) => {
+      {users.map((userData) => {
         return (
-          <div key={userData.id} className="grid grid-cols-2 mb-8 m-auto mt-8 bg-slate-950 text-white  text-center w-4/5">
+          <div key={userData.id} className="grid grid-cols-2 mb-8 m-auto mt-8 bg-white text-black text-center w-4/5 shadow-lg">
             <div>
-              <img src={userData.imgURL} alt="user avatar" />
+              <img src={userData.imgURL}/>
             </div>
 
             <div className="mt-8 mr-32 py-4 w-4/5">
               <h3 className="text-2xl mb-4">{userData.name}</h3>
               <h3 className="text-2xl mb-4">{userData.ocupation}</h3>
-              <p className="text-xl"><strong>{userData.biography}</strong></p>
+              <p className="flex flex-wrap text-xl  leading-7 "><strong>{userData.biography}</strong></p>
             </div>
           </div>
         );
